69ee879db8348cf7c2fb0484e213419c228726f6
Arowo <Arowobusoyesaanu088@gmail.com>
2024-02-28 07:41:17 -0800

    shell

--- shell.h
+++ shell.h
@@ -1,74 +1,38 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <stddef.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include "str.h"
 
-#define MAX_COMMAND_LENGTH 100
+void print_prompt(void);
+void parser(char *buffer, char **cmd);
+int add_path(char **cmd, char **env);
+void print_env(char **env);
+int exit_arg222(char **cmd, char **argv, char *buffer);
+int exit_arg(char **args);
+int REPL(char *buf, size_t buf_len, char **cmd, char **argv, char **env);
+int get_arr_len(char **str);
+unsigned int check_delim(char c, const char *str);
+int _atoi(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *str1, char *str2);
+int read_cmd(char **buffer, size_t *buf_len);
+void execute_cmd(char **cmd, char **argv, char **env);
+int _printf(char *);
+char *_strcat(char *dest, char *src);
+int _strlen(char *str);
+char *_strdup(char *str);
+char *build_path(char *token, char *value);
+char *_memset(char *str, char c, unsigned int n);
+char *_getenv(char *buf, char **env_dir);
+int _strncmp(char *str1, char *str2, unsigned int n);
+char *_strtok(char *str, const char *delim);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-void display_prompt()
-{
-    printf("#cisfun$ ");
-    fflush(stdout);
-}
-
-void execute_command(char *command)
-{
-    if (access(command, X_OK) == 0)
-    {
-        pid_t pid = fork();
-
-        if (pid == -1)
-        {
-            perror("fork");
-            return;
-        }
-        else if (pid == 0)
-        {
-            // Child process
-            char *args[] = {command, NULL};
-            execve(command, args, NULL);
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            // Parent process
-            int status;
-            waitpid(pid, &status, 0);
-        }
-    }
-    else
-    {
-        printf("%s: No such file or directory\n", command);
-    }
-}
-
-int main()
-{
-    char command[MAX_COMMAND_LENGTH];
-
-    while (1)
-    {
-        display_prompt();
-
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-        {
-            printf("\n");
-            break;
-        }
-
-        command[strcspn(command, "\n")] = '\0'; // Remove the newline character
-
-        if (strcmp(command, "exit") == 0)
-        {
-            break;
-        }
-
-        execute_command(command);
-    }
-
-    return 0;
-}
+#endif
